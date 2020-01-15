15ea498f4e68b6eb1adb54add7f86e98b02c9e86
Interviews <interviews@dj.local>
2020-01-15 11:50:24 +0300

    Initial Commit

---
+++ AndelaInterview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AndelaInterview
+//
+//  Created by Interviews on 15/01/2020.
+//  Copyright © 2020 Interviews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
